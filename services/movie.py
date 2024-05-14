7b0c834a01195f3db365168f77156448d3310b91
semermaks <semermaks@mail.com>
2024-05-14 20:14:56 +0300

    related_names fix and formatting fix

--- services/movie.py
+++ services/movie.py
@@ -29,7 +29,8 @@ def create_movie(
 ) -> None:
     movie = Movie.objects.create(
         title=movie_title,
-        description=movie_description)
+        description=movie_description,
+    )
     if genres_ids:
         movie.genres.set(genres_ids)
     if actors_ids:
